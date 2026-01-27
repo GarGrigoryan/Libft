cb5a8d0bfdc30f03f35d7049879099028e6ae5ac
Garik Grigoryan <gargrigo@c2r18s6.42yerevan.am>
2026-01-27 18:07:18 +0400

    start

---
+++ libft.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gargrigo <gargrigo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/26 18:04:41 by gargrigo          #+#    #+#             */
+/*   Updated: 2026/01/26 19:28:22 by gargrigo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
+int	ft_strlen(int c);
+
+#endif
