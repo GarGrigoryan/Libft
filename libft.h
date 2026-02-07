fe6bd20bf7e4b37125f686fb54775e59953be482
GarGrigoryan <garikyang950@gmail.com>
2026-02-08 01:53:05 +0400

    ft_striteri added

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: garik <garik@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/26 18:04:41 by gargrigo          #+#    #+#             */
-/*   Updated: 2026/02/08 01:47:22 by garik            ###   ########.fr       */
+/*   Updated: 2026/02/08 01:52:54 by garik            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ int				ft_tolower(int c);
 int				ft_strncmp(char *s1, char *s2, unsigned int n);
 int				ft_memcmp(const void *s1, const void *s2, size_t n);
 int				ft_atoi(char *str);
+void            ft_striteri(char *s, void (*f)(unsigned int, char*));
 void			*ft_memset(void *s, int c, size_t n);
 void			*ft_bzero(void *s, size_t n);
 void			*ft_memcpy(void *dest, const void *src, size_t n);
