8e97c79062c2f39c7ee859f7807ba0a48a30aa1d
GarGrigoryan <garikyang950@gmail.com>
2026-02-08 23:03:13 +0400

    Add ft_lstadd_back function and update header

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: garik <garik@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/26 18:04:41 by gargrigo          #+#    #+#             */
-/*   Updated: 2026/02/08 22:22:51 by garik            ###   ########.fr       */
+/*   Updated: 2026/02/08 22:27:35 by garik            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,7 @@ void            ft_putstr_fd(char *s, int fd);
 void            ft_putendl_fd(char *s, int fd);
 void            ft_putnbr_fd(int n, int fd);
 void            ft_lstadd_front(t_list **lst, t_list *new);
+void            ft_lstadd_back(t_list **lst, t_list *new);
 void			*ft_memset(void *s, int c, size_t n);
 void			*ft_bzero(void *s, size_t n);
 void			*ft_memcpy(void *dest, const void *src, size_t n);
