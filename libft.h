5c1ce7cc2d8cad33376a76f8e96dd1e5f1a6577b
GarGrigoryan <garikyang950@gmail.com>
2026-02-08 20:37:59 +0400

    ft_putchar_fd added

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: garik <garik@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/26 18:04:41 by gargrigo          #+#    #+#             */
-/*   Updated: 2026/02/08 01:52:54 by garik            ###   ########.fr       */
+/*   Updated: 2026/02/08 20:37:17 by garik            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ int				ft_strncmp(char *s1, char *s2, unsigned int n);
 int				ft_memcmp(const void *s1, const void *s2, size_t n);
 int				ft_atoi(char *str);
 void            ft_striteri(char *s, void (*f)(unsigned int, char*));
+void            ft_putchar_fd(char c, int fd);
 void			*ft_memset(void *s, int c, size_t n);
 void			*ft_bzero(void *s, size_t n);
 void			*ft_memcpy(void *dest, const void *src, size_t n);
